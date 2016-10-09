2e4e60b235698005221ea0033409a1c2150ecee3
PanJing <j.pan@someway.me>
2016-10-09 14:59:05 +0800

    new

--- js/index.js
+++ js/index.js
@@ -1,9 +1,4 @@
-$(function(){
-	var _hmt = _hmt || [];
-	(function() {
-	var hm = document.createElement("script");
-	//hm.src = "//hm.baidu.com/hm.js?73c27e26f610eb3c9f3feb0c75b03925";
-	var s = document.getElementsByTagName("script")[0];
-	s.parentNode.insertBefore(hm, s);
-	})();
+$("#top_nav").find("li").click(function(){
+	$("#top_nav").find("li").removeClass("active");
+	$(this).addClass("active");
 })
